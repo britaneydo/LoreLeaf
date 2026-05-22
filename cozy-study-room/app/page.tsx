b16ac8bf349a6f52da067e12d4860c445a69596d
Richard <149290741+rtledev@users.noreply.github.com>
2026-05-21 21:42:44 -0700

    fetching data from supabase and updating tree_point xp

--- cozy-study-room/app/page.tsx
+++ cozy-study-room/app/page.tsx
@@ -1,65 +1,93 @@
-import Image from "next/image";
+"use client";
 
+import { useEffect, useState} from "react";
+import { supabase } from "@/lib/supabaseClient"
+
+// loads current tree data from supabase and reads tree_progression table
 export default function Home() {
+  const [points, setPoints] = useState(0);
+  const [stage, setStage] = useState("seed");
+  const [message, setMessage] = useState("");
+
+  async function loadTree() {
+    const { data, error } = await supabase
+      .from("tree_progression")
+      .select("total_points, tree_stage")
+      .eq("id", 1)  // req tree row where id = 1
+      .single(); // one row is expected
+  
+
+  // log error
+  if (error) {
+    console.error("Error loading tree: ", error);
+    // updates front-end
+    setMessage("Could not load tree data.");
+
+    return;
+  }
+
+  setPoints(data.total_points);
+  setStage(data.tree_stage);
+}
+
+// calls postgreSQL func add_tree_points()
+async function add_tree_points() {
+  // Calls RPC func
+  const { data, error } = await supabase.rpc('add_tree_points', {
+    points_to_add: 1,
+  });
+
+  console.log("RPC Data:", data);
+  console.log("RPC Error:", error);
+
+  // log error
+  if (error) {
+    console.error("Error adding point: ", error);
+    // updates front-end
+    setMessage("Point could not be added.");
+
+    return;
+  }
+
+  // Updates the page directly from the returned SQL data.
+  if (data && data.length > 0) {
+    setPoints(data[0].new_total_points);
+    setStage(data[0].new_tree_stage);
+  }
+
+  // reloads tree data after successful update
+  await loadTree();
+
+  setMessage("Added +1 point !");
+  
+}
+  // runs once the page loads and the [] meanas that it only runs on first render
+  useEffect(() => {
+  // Creates an async function inside useEffect to avoid unnecessary renders
+  async function fetchTreeOnLoad() {
+    // Calls our Supabase loading function.
+    await loadTree();
+  }
+
+  // Runs the async function.
+  fetchTreeOnLoad();
+}, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Cozy Study Room</h1>
+
+      <p>Tree Points: {points}</p>
+      <p>Tree Stage: {stage}</p>
+
+      <button
+        onClick={add_tree_points}
+        className="rounded-lg bg-green-700 px-4 py-2 text-white"
+      >
+        Add +1 Tree Point
+      </button>
+
+      <p>{message}</p>
+    </main>
   );
 }
